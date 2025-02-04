b9f6b423a440bff4a26ba3a5deada1371d7254e1
josdic1 <joshuadicker@gmail.com>
2025-02-04 13:12:05 -0500

    completed the CRUD functions with separation of concerns, allowing everything to fire once. Going to make a filter now, and then submit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 const init = () => {
+  let inEditMode = false;
   let questions = [];
   let formData = {};
+  let selectedQuestion = {
+    id: '',
+    question: '',
+    answer: '',
+    difficulty: ''
+  }
 
   const message = document.getElementById("message");
   const list = document.getElementById("list");
@@ -14,6 +21,9 @@ const init = () => {
         <td>${q.id}</td>
         <td class='td-hover' id="${q.id}">${q.question}</td>
           <td>${q.difficulty}</td>
+             <td>
+          <button type="button" name="edit" id="${q.id}"> O </button>
+        </td>
         <td>
           <button type="button" name="del" id="${q.id}"> X </button>
         </td>
@@ -26,6 +36,8 @@ const init = () => {
           <tr>
             <th>ID</th>
             <th>Question</th>
+            <th>Level</th>
+            <th>Edit</th>
             <th>Del</th>
           </tr>
         </thead>
@@ -53,7 +65,7 @@ const init = () => {
   function renderForm() {
     const formHtml = `
       <input type='text' id='question' name='question' placeholder='Question goes here...' />
-           <input type='text' id='answer' name='answer' placeholder='Answer goes here...' /><br>
+           <input type='text' id='answer' name='answer' placeholder='Answer goes here...' />
            <select id='difficulty' name='difficulty'>
             <option value = '' selected disabled>Choose one...</option>
              <option value = 'easy'>Easy</option>
@@ -61,26 +73,73 @@ const init = () => {
              <option value = 'hard'>Hard</option>
              <option value = 'impossible'>Impossible</option>
            </select>
-      <button type='submit' id='submit' name='submit'> Submit </button>
+      <button type='submit' class='submit' id='submit' name='submit'> Submit </button>
+      <button type='button' id='clear' name='clear'> Clear </button>
     `;
     form.innerHTML = formHtml;
 
-    form.addEventListener("submit", async function (e) {
-      e.preventDefault();
-      const qVal = document.getElementById("question").value;
-      const aVal = document.getElementById("answer").value;
-      const dVal = document.getElementById("difficulty").value;
-      formData = {
+    const clearBtn = document.getElementById('clear')
+    clearBtn.addEventListener('click', function (e) {
+      if (e.target.id === 'clear') {
+        clearForm()
+      }
+    })
+
+    form.addEventListener('submit', handleSubmitClick)
+  }
+
+  list.addEventListener("click", function (e) {
+    if (e.target.name === 'del') {
+      const qId = e.target.id;
+      deleteQs(qId);
+    } else {
+      if (e.target.name === 'edit') {
+        inEditMode = true
+        const qId = e.target.id;
+        const obj = questions.find(q => q.id === qId)
+        selectedQuestion = obj
+        document.getElementById("question").value = obj.question
+        document.getElementById("answer").value = obj.answer
+        document.getElementById("difficulty").value = obj.difficulty
+      }
+    }
+  });
+
+  async function handleSubmitClick(e) {
+    e.preventDefault();
+    const qVal = document.getElementById("question").value;
+    const aVal = document.getElementById("answer").value;
+    const dVal = document.getElementById("difficulty").value;
+    if (!inEditMode) {
+      const newQuestion = {
         ...formData,
         question: qVal,
         answer: aVal,
         difficulty: dVal
       };
-      await addQs(formData);
-    });
-
+      await addQs(newQuestion);
+      formData = newQuestion
+    } else {
+      const updatedQuestion = {
+        ...selectedQuestion,
+        id: selectedQuestion.id,
+        question: qVal,
+        answer: aVal,
+        difficulty: dVal
+      };
+      await updateQs(updatedQuestion);
+      selectedQuestion = updatedQuestion
+    }
   }
 
+
+
+  function clearForm() {
+    document.getElementById("question").value = ''
+    document.getElementById("answer").value = ''
+    document.getElementById("difficulty").value = ''
+    document.getElementById("message").textContent = ''
+  }
 
 
   async function fetchQs() {
@@ -117,6 +176,24 @@ const init = () => {
     }
   }
 
+  async function updateQs(question) {
+    const qId = question.id
+    try {
+      console.log("Trying to update ID:", qId);
+      const r = await fetch(`http://localhost:3000/questions/${qId}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(question)
+      });
+      if (!r.ok) {
+        throw new Error(`Update failed: ${r.status} ${r.statusText}`);
+      }
+      await fetchQs()
+    } catch (error) {
+      console.error("Error updating question:", error);
+    }
+  }
+
   async function addQs(newQ) {
     try {
       const r = await fetch("http://localhost:3000/questions", {
@@ -134,12 +211,9 @@ const init = () => {
     }
   }
 
-  list.addEventListener("click", function (e) {
-    if (e.target.name === 'del') {
-      const qId = e.target.id;
-      deleteQs(qId);
-    }
-  });
+
 };
+
+
 
 window.addEventListener("DOMContentLoaded", init);
